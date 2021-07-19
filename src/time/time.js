1aab596c7cce5b0c88877088854eebdb167353a1
Suryapratap Singh <suryprtaps@gmail.com>
2021-07-19 14:22:49 +0530

    add time.js file in time category

---
+++ src/time/time.js
@@ -0,0 +1,20 @@
+// time conversion methods.
+
+// nanosecond to micorsecond.
+let nsToUs = (ns) => ns / 1000;
+// nanosecond to milisecond.
+let nsToMs = (ns) => ns / 1e+6;
+// nanosecond to second.
+let nsToSc = (ns) => ns / 1e+9;
+// nanosecond to minute.
+let nsToMi = (ns) => ns / 6e+10;
+// nanosecond to hour.
+let nsToHr = (ns) => ns / 3.6e+12;
+// nanosecond to day.
+let nsToDd = (ns) => ns / 8.64e+13;
+// nanosecond to week.
+let nsToWk = (ns) => ns / 6.048e+14;
+// nanosecond to month.
+let nsToMm = (ns) => ns / 2.628e+15;
+// nanosecond to year.
+let nsToYy = (ns) => ns / 3.154e+16;
