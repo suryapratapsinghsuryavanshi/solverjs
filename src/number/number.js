dd7ab0b28edd72b920d1e28b22e1160bd4ef8e08
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-22 21:56:14 +0530

    fix doctype in number system

--- src/number/number.js
+++ src/number/number.js
@@ -1,7 +1,7 @@
 /**
  * The hacToDec methods return hexadecimal to decimal numbers.
  * @param {String} hex Hex value for conversion.
- * @returns {Number} It returnd hex equivalent decimal value.
+ * @returns {Number} It returned a hex equivalent decimal value.
  */
 let hexToDec = (hex) => parseInt(hex, 16)
 
@@ -9,7 +9,7 @@ let hexToDec = (hex) => parseInt(hex, 16)
 /**
  * The hacToOct methods return hexadecimal to Octal numbers.
  * @param {String} hex Hex value for conversion.
- * @returns {String} It returnd hex equivalent ocatal value.
+ * @returns {String} It returned a hex equivalent ocatal value.
  */
 let hexToOct = (hex) => parseInt(hex, 16).toString(8).toUpperCase();
 
@@ -17,7 +17,7 @@ let hexToOct = (hex) => parseInt(hex, 16).toString(8).toUpperCase();
 /**
  * The hacToBin methods return hexadecimal to binary numbers.
  * @param {String} hex Hex value for conversion.
- * @returns {String} It returnd hex equivalent binary value.
+ * @returns {String} It returned a hex equivalent binary value.
  */
 let hexToBin = (hex) => parseInt(hex, 16).toString(2).toUpperCase();
 
@@ -25,7 +25,7 @@ let hexToBin = (hex) => parseInt(hex, 16).toString(2).toUpperCase();
 /**
  * The decToHex methods return decimal to hexadecimal numbers.
  * @param {Number} dec Dec value for conversion.
- * @returns {String} It returnd dec equivalent hexadecimal value.
+ * @returns {String} It returned a dec equivalent hexadecimal value.
  */
 let decToHex = (dec) => dec.toString(16);
 
@@ -33,7 +33,7 @@ let decToHex = (dec) => dec.toString(16);
 /**
  * The decToOct methods return decimal to Octal numbers.
  * @param {Number} dec Dec value for conversion.
- * @returns {String} It returnd dec equivalent octal value.
+ * @returns {String} It returned a dec equivalent octal value.
  */
 let decToOct = (dec) => dec.toString(8);
 
@@ -41,7 +41,7 @@ let decToOct = (dec) => dec.toString(8);
 /**
  * The decToBin methods return decimal to binary numbers.
  * @param {Number} dec Dec value for conversion.
- * @returns {String} It returnd dec equivalent binary value.
+ * @returns {String} It returned a dec equivalent binary value.
  */
 let decToBin = (dec) => dec.toString(2);
 
@@ -50,7 +50,7 @@ let decToBin = (dec) => dec.toString(2);
 /**
  * The octToHex methods return octal to hexadecimal numbers.
  * @param {String} oct Oct value for conversion.
- * @returns {String} It returnd oct equivalent hexadecimal value.
+ * @returns {String} It returned a oct equivalent hexadecimal value.
  */
 let octToHex = (oct) => parseInt(oct, 8).toString(16).toUpperCase();
 
@@ -58,7 +58,7 @@ let octToHex = (oct) => parseInt(oct, 8).toString(16).toUpperCase();
 /**
  * The octToBin methods return octal to decimal numbers.
  * @param {String} oct Oct value for conversion.
- * @returns {Number} It returnd oct equivalent decimal value.
+ * @returns {Number} It returned a oct equivalent decimal value.
  */
 let octToDec = (oct) => parseInt(oct, 8);
 
@@ -66,7 +66,7 @@ let octToDec = (oct) => parseInt(oct, 8);
 /**
  * The octToBin methods return octal to binary numbers.
  * @param {String} oct Oct value for conversion.
- * @returns {String} It returnd oct equivalent binary value.
+ * @returns {String} It returned a oct equivalent binary value.
  */
 let octToBin = (oct) => parseInt(oct, 8).toString(2).toUpperCase();
 
@@ -74,7 +74,7 @@ let octToBin = (oct) => parseInt(oct, 8).toString(2).toUpperCase();
 /**
  * The binToHex methods return binary to hexadecimal numbers.
  * @param {String} bin Binary value for conversion.
- * @returns {String} It returnd binary equivalent hexadecimal value.
+ * @returns {String} It returned a binary equivalent hexadecimal value.
  */
 let binToHex = (bin) => parseInt(bin, 2).toString(16).toUpperCase();
 
@@ -82,7 +82,7 @@ let binToHex = (bin) => parseInt(bin, 2).toString(16).toUpperCase();
 /**
  * The binToDec methods return binary to decimal numbers.
  * @param {String} bin Binary value for conversion.
- * @returns {Number} It returnd binary equivalent decimal value.
+ * @returns {Number} It returned a binary equivalent decimal value.
  */
 let binToDec = (bin) => parseInt(bin, 2);
 
@@ -90,7 +90,7 @@ let binToDec = (bin) => parseInt(bin, 2);
 /**
  * The binToOct methods return binary to octal numbers.
  * @param {String} bin Binary value for conversion.
- * @returns {String} It returnd binary equivalent octal value.
+ * @returns {String} It returned a binary equivalent octal value.
  */
 let binToOct = (bin) => parseInt(bin, 2).toString(8).toUpperCase();
 
